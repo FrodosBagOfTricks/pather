a7a0bfeb0b11ae64d4e05f44b2bd7176f6d88b89
Frodo <Martin.Stewart@Systemc.com>
2022-08-20 19:11:46 +0100

    Added FAB

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,52 @@
-<script setup lang="ts">
-import TheMap from "./components/TheMap.vue";
-</script>
-
 <template>
   <main>
     <TheMap />
+
+    <div class="dial">
+      <SpeedDial
+        :model="items"
+        :radius="120"
+        direction="up-left"
+        type="quarter-circle"
+        buttonClass="p-button-success"
+      />
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { ref } from "vue";
+import TheMap from "./components/TheMap.vue";
+
+const items = ref([
+  {
+    label: "Add",
+    icon: "pi pi-pencil",
+    command: () => {
+      console.log("Add");
+    },
+  },
+  {
+    label: "Update",
+    icon: "pi pi-refresh",
+    command: () => {
+      console.log("Update");
+    },
+  },
+  {
+    label: "Delete",
+    icon: "pi pi-trash",
+    command: () => {
+      console.log("Delete");
+    },
+  },
+]);
+</script>
+
+<style scoped>
+.dial {
+  position: fixed;
+  top: calc(100vh - 6rem);
+  left: calc(100vw - 6rem);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+import PrimeVue from "primevue/config";
+import Speeddial from "primevue/speeddial";
+
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component("SpeedDial", Speeddial);
+
+app.mount("#app");
